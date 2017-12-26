7d2840d90335e9c07bb17a8dce0b7974a17469e7
abhishekarohi <arohiabhi@gmail.com>
2017-12-26 14:11:12 -0600

    URI and URL conversions

--- Networking/src/Netwroking.java
+++ Networking/src/Netwroking.java
@@ -6,20 +6,29 @@ public class Netwroking {
 	{
 		try
 		{
-			URI uri = new URI("db://username:password@myserver.com:5000/catalouge/phones?os=iOS#apple");
+//			URI uri = new URI("http://username:password@myserver.com:5000/catalouge/phones?os=iOS#apple");
+			URI baseUri = new URI("http://username:password@myserver.com:5000");
+			URI uri = new URI("/catalouge/phones?os=iOS#apple");
+//			URL url = uri.toURL();
+			URL url= baseUri.resolve(uri).toURL();
 			
-			System.out.println("Scheme = " + uri.getScheme());
-			System.out.println("Scheme-Specific Part = " + uri.getSchemeSpecificPart());
-			System.out.println("Authority = " + uri.getAuthority());
-			System.out.println("User Info = " + uri.getUserInfo());
-			System.out.println("Host = " + uri.getHost());
-			System.out.println("Port = " + uri.getPort());
-			System.out.println("Path = " + uri.getPath());
-			System.out.println("Query = " + uri.getQuery());
-			System.out.println("Fragment = " + uri.getFragment());
+			System.out.println("URL = " + url);
+			
+//			System.out.println("Scheme = " + uri.getScheme());
+//			System.out.println("Scheme-Specific Part = " + uri.getSchemeSpecificPart());
+//			System.out.println("Authority = " + uri.getAuthority());
+//			System.out.println("User Info = " + uri.getUserInfo());
+//			System.out.println("Host = " + uri.getHost());
+//			System.out.println("Port = " + uri.getPort());
+//			System.out.println("Path = " + uri.getPath());
+//			System.out.println("Query = " + uri.getQuery());
+//			System.out.println("Fragment = " + uri.getFragment());
 			
 		}catch (URISyntaxException e)
 		{
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
