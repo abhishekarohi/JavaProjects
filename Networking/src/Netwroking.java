ef37622722624c01c79d5809386ac1132160213e
abhishekarohi <arohiabhi@gmail.com>
2017-12-26 22:46:21 -0600

    Chapter Complete

--- Networking/src/Netwroking.java
+++ Networking/src/Netwroking.java
@@ -1,37 +1,48 @@
+import java.io.*;
 import java.net.*;
 
 public class Netwroking {
 
 	public static void main(String[] args) 
 	{
-		try
-		{
-//			URI uri = new URI("http://username:password@myserver.com:5000/catalouge/phones?os=iOS#apple");
-			URI baseUri = new URI("http://username:password@myserver.com:5000");
-			URI uri = new URI("/catalouge/phones?os=iOS#apple");
-//			URL url = uri.toURL();
-			URL url= baseUri.resolve(uri).toURL();
+		try 
+			{
+				URL url = new URL("http://example.org");
+				HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+				urlConnection.setRequestMethod("GET");
+				urlConnection.setRequestProperty("User-Agent", "Chrome");
+				urlConnection.setReadTimeout(30000);
+				
+				int respCode = urlConnection.getResponseCode();
+				System.out.println("Responce Code " + respCode);
+				
+				
+				if (respCode != 200)
+				{
+					System.out.println("Error reading the web page");
+					return;
+				}
+				
+				BufferedReader inStream = new BufferedReader(
+						new InputStreamReader(urlConnection.getInputStream()));
+				String inputLine = "";
+				
+				while(inputLine != null)
+				{
+					inputLine = inStream.readLine();
+					System.out.println(inputLine);					
+				}
+				inStream.close();		
 			
-			System.out.println("URL = " + url);
-			
-//			System.out.println("Scheme = " + uri.getScheme());
-//			System.out.println("Scheme-Specific Part = " + uri.getSchemeSpecificPart());
-//			System.out.println("Authority = " + uri.getAuthority());
-//			System.out.println("User Info = " + uri.getUserInfo());
-//			System.out.println("Host = " + uri.getHost());
-//			System.out.println("Port = " + uri.getPort());
-//			System.out.println("Path = " + uri.getPath());
-//			System.out.println("Query = " + uri.getQuery());
-//			System.out.println("Fragment = " + uri.getFragment());
-			
-		}catch (URISyntaxException e)
-		{
-			e.printStackTrace();
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
+			} 
+			catch (MalformedURLException e) 
+			{			
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+	}		
 
 }
